8861f390652afad6eb0ccee4ee97429e2ec8c536
akmalip <134563320+akmalip@users.noreply.github.com>
2026-04-17 22:07:51 +0700

    Delete src directory

--- src/components/PageType.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useEffect } from "react";
-export default function PageType({ type }: { type: "home" | "inner" }) {
-  useEffect(() => {
-    document.documentElement.setAttribute("data-page", type);
-    return () => document.documentElement.removeAttribute("data-page");
-  }, [type]);
-  return null;
-}
